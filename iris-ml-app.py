92c096999d7805d480c6eeb7ae1bad3c89aaf35a
nurazua <111936415+nurazua@users.noreply.github.com>
2022-08-24 14:24:54 +0800

    Update iris-ml-app.py

--- iris-ml-app.py
+++ iris-ml-app.py
@@ -47,5 +47,8 @@ st.write(iris.target_names[prediction])
 
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
+
+from PIL import Image
+image = Image.open('iris image.png')
+
+st.image(image, caption='Iris Photography')
